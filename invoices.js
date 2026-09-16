54f926583fa2271597fd9729aefba55fee66c7af
perryoust <perryoust2@gmail.com>
2026-09-16 08:53:29 +0300

    Fix budget panel display bug v111.83

--- invoices.js
+++ invoices.js
@@ -5,7 +5,7 @@
 
 let _appMode = 'act'; // 'act' | 'purch'
 let _purchTab = 'pdash';
-const PURCH_TABS = ['pdash','pinvoices','porders','pdeliveries','psup'];
+const PURCH_TABS = ['pdash','pinvoices','porders','pdeliveries','psup','pbudget'];
 
 // --- Debounced rendering for performance ---
 window.debouncedRenderInvoices = window.debounce ? window.debounce(() => {
